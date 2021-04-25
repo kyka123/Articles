f02d741dce00e97c010ffa784a2f7df6a1961ce2
Bart Slepecki <gguess1337@gmail.com>
2021-04-25 14:13:41 +0200

    fixed footer, fixed hero section layout

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,23 +3,37 @@ import { IoLogoFacebook } from "react-icons/io";
 import { IoLogoInstagram } from "react-icons/io";
 import { IoLogoYoutube } from "react-icons/io";
 
-
 function Footer() {
   return (
-        <div className={styles.main}>
-          {/* <img src="assets/footer.jpg" alt="IPG" className={styles.image} /> */}
-          <div className={styles.icons_text}>
-            <div className={styles.icons}>
-            <a className={styles.a} target="_blank" href="https://www.facebook.com/ALO.rzeszow/"><IoLogoFacebook className={styles.fb_icon}/></a>
-            <a className={styles.a} target="_blank" href="https://www.instagram.com/alo_rzeszow/?hl=pl"><IoLogoInstagram className={styles.ig_icon}/></a>
-            <a className={styles.a} target="_blank" href="https://www.youtube.com/channel/UCXaHPxICvAtYZUIlt9oMxsw/videos"><IoLogoYoutube className={styles.yt_icon}/></a>
-            </div>
+    <div className={styles.main}>
+      <div className={styles.icons_text}>
+        <div className={styles.icons}>
+          <a
+            className={styles.a}
+            target="_blank"
+            href="https://www.facebook.com/ALO.rzeszow/"
+          >
+            <IoLogoFacebook className={styles.icon} />
+          </a>
+          <a
+            className={styles.a}
+            target="_blank"
+            href="https://www.instagram.com/alo_rzeszow/?hl=pl"
+          >
+            <IoLogoInstagram className={styles.icon} />
+          </a>
+          <a
+            className={styles.a}
+            target="_blank"
+            href="https://www.youtube.com/channel/UCXaHPxICvAtYZUIlt9oMxsw/videos"
+          >
+            <IoLogoYoutube className={styles.icon} />
+          </a>
+        </div>
 
-            <p className={styles.text}>© 2021. All rights reserved</p>
-          </div>
-                      
-          </div>
-
+        <p className={styles.text}>© 2021. All rights reserved</p>
+      </div>
+    </div>
   );
 }
 
